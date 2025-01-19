6c4df86c57cb11355e0598daaaa590f2c58e2905
sagar <sagar@sagars-MacBook-Air.local>
2025-01-19 11:56:05 +0000

    updated code for Day2

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const sliderItems = [
   {
     id: "1",
     img: "assets/braclet/main.jfif",
-    title: "Helm charts have been successfully configured using ArgoCD",
+    title: "Day2 Dockerized React Application successfully ",
     desc: "This is now in new commit",
     bg: "green",
   },
