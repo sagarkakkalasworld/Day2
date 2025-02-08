8251888a820fc7fdf0be65b5ac3f9d3d191c6a11
sagarkakkalasworld <sagarkakkalasworld@gmail.com>
2025-02-08 15:01:33 +0000

    Update data.js

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const sliderItems = [
   {
     id: "1",
     img: "assets/braclet/main.jfif",
-    title: "Day2 Dockerized React Application successfully ",
+    title: "Automated Build and Deployemnt using Bash successfully",
     desc: "This is now in new commit",
     bg: "green",
   },
